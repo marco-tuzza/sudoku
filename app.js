f542f8b0613877268780a31184af43a09c160161
Marco Tuzza <marcotuzza@hotmail.it>
2022-11-26 03:23:54 +0100

    basic-functionality
    Added the possibility to write a number in a cell and to remove it

---
+++ app.js
@@ -0,0 +1,28 @@
+$( ".cell" ).on( "click", function() {
+    $(".cell").removeClass("active");
+    $(this).addClass("active");
+});
+
+$( ".number" ).on( "click", function() {
+    var number = $(this).text();
+
+    $(".cell.active > span").text(number)
+    $(".cell.active").removeClass("active");
+});
+
+$( ".bin" ).on( "click", function() {
+    $(".cell.active > span").text("")
+    $(".cell.active").removeClass("active");
+});
+
+$(document).mouseup(function(e) 
+{
+    var container = $(".board");
+    var numbers = $(".numbers");
+
+    // if the target of the click isn't the container nor a descendant of the container
+    if (!container.is(e.target) && !numbers.is(e.target) && container.has(e.target).length === 0 && numbers.has(e.target).length === 0) 
+    {
+        $(".cell").removeClass("active");
+    }
+});
