769cb0c62b9642a76ef0ccfe87cfef408f7244bb
Marco Tuzza <marcotuzza@hotmail.it>
2022-12-08 23:38:03 +0100

    Cleaned up code and improved existing functions

--- app.js
+++ app.js
@@ -1,105 +1,135 @@
-$( ".cell" ).on( "click", function() {
-    $(".cell").removeClass("active");
-    $(this).addClass("active");
+// Triggers
+$(".cell").click(activeCell);
+$(".number").click(insertNumber);
+$(".bin").click(deleteNumber);
+$(document).mouseup(removeActive);
 
-    const activeCell = $(".cell.active");
-    const row = activeCell.data("row");
-    const column = activeCell.data("column");
-
-    $(".cell").removeClass("soft-active");
-
-    var rowElements = $('.cell[data-row="' + row + '"]');
-    var columnElements = $('.cell[data-column="' + column + '"]');
-
-    $(rowElements).addClass("soft-active");
-    $(columnElements).addClass("soft-active");
-});
-
-$( ".number" ).on( "click", function() {
-    var number = $(this).text();
-    var number = parseInt(number);
-
-    if (!$(".cell.active").hasClass("blocked")) 
-    {
-        $(".cell.active > span").text(number)
-        checkErrors(number)
-        $(".cell.active").attr("data-value", number)
-    }
-});
-
-$( ".bin" ).on( "click", function() {
-    $(".cell.active > span").text("")
-    $(".cell.active").removeClass("error")
-
-    const activeCell = $(".cell.active");
-    const square = activeCell.data("square");
-    const value = activeCell.data("value");
-    const row = activeCell.data("row");
-    const column = activeCell.data("column");
-
-    var squareElements = $('.cell[data-square="' + square + '"]');
-    squareElements.removeClass('error');
-    var element = $('.cell[data-square="' + square + '"][data-value="' + value + '"]');
-
-    if ($(element).length) {
-        $(element).removeClass("error");
-    }
-
-    var rowElements = $('.cell[data-row="' + row + '"]');
-    rowElements.removeClass('error');
-    var element = $('.cell[data-row="' + row + '"][data-value="' + value + '"]');
-
-    if ($(element).length) {
-        $(element).removeClass("error");
-    }
-
-    var rowElements = $('.cell[data-column="' + column + '"]');
-    rowElements.removeClass('error');
-    var element = $('.cell[data-row="' + column + '"][data-value="' + value + '"]');
-
-    if ($(element).length) {
-        $(element).removeClass("error");
-    }
-
-    $(".cell.active").attr("data-value", 0)
-});
-
-$(document).mouseup(function(e) 
+function activeCell() 
 {
-    var container = $(".board");
-    var numbers = $(".numbers");
-
-    // if the target of the click isn't the container nor a descendant of the container
-    if (!container.is(e.target) && !numbers.is(e.target) && container.has(e.target).length === 0 && numbers.has(e.target).length === 0) 
+    // Get the active cell
+    var activeCell = $(this);
+  
+    // Check if the active cell has the "data-row" and "data-column" attributes set
+    if (activeCell.attr("data-row") && activeCell.attr("data-column")) 
     {
-        $(".cell").removeClass("active");
-    }
-});
-
-function checkErrors (number) 
-{
-    const activeCell = $(".cell.active");
-    const square = activeCell.data("square");
-    const row = activeCell.data("row");
-    const column = activeCell.data("column");
-    const value = number;
-
-    var squareElements = $('.cell[data-square="' + square + '"]');
-    squareElements.removeClass('error');
-    var elementSquare = $('.cell[data-square="' + square + '"][data-value="' + value + '"]');
+        // Remove the "active" class from all cells
+        $(".cell.active").removeClass("active");
     
-    var rowElements = $('.cell[data-row="' + row + '"]');
-    rowElements.removeClass('error');
-    var elementRow = $('.cell[data-row="' + row + '"][data-value="' + value + '"]');
-
-    var columnElements = $('.cell[data-column="' + column + '"]');
-    columnElements.removeClass('error');
-    var elementColumn = $('.cell[data-column="' + column + '"][data-value="' + value + '"]');
+        // Remove the "soft-active" class from all cells
+        $(".cell.soft-active").removeClass("soft-active");
     
-    if (elementSquare.length || elementRow.length || elementColumn.length) {
-        activeCell.addClass("error")
-        elementSquare.addClass("error")
-        elementRow.addClass("error")
-        elementColumn.addClass("error")
+        // Add the "active" class to the active cell
+        activeCell.addClass("active");
+    
+        // Get the row and column of the active cell
+        var row = activeCell.data("row");
+        var column = activeCell.data("column");
+    
+        // Select all cells in the same row and column as the active cell
+        var rowElements = $('.cell[data-row="' + row + '"]');
+        var columnElements = $('.cell[data-column="' + column + '"]');
+    
+        // Add the "soft-active" class to the selected cells
+        $(rowElements).addClass("soft-active");
+        $(columnElements).addClass("soft-active");
     }
 }
+
+function insertNumber() 
+{
+    // Take the active cell
+    var cell = $(".cell.active");
+
+    // Convert the number to an integer
+    var number = parseInt($(this).text())
+
+    // Check if the cell has the "blocked" class
+    if (!cell.hasClass("blocked")) 
+    {
+    // Check if the number is valid (i.e. an integer between 1 and 9)
+        if (Number.isInteger(number) && number >= 1 && number <= 9) 
+        {
+            // Insert the number into the cell
+            cell.find('span').text(number);
+    
+            // Set the "data-value" attribute of the cell to the number
+            cell.attr("data-value", number);
+    
+            // Check for errors
+            checkErrors();
+        } 
+        else 
+        {
+            // Add the "error" class to the cell
+            cell.addClass("error");
+        }
+    }
+}
+
+function deleteNumber() 
+{
+    // Check if the currently selected cell has the "blocked" class
+    if (!$(".cell.active").hasClass("blocked")) 
+    {
+        // Remove the content of the currently selected cell
+        $(".cell.active > span").text("");
+    
+        // Remove the "error" class from the currently selected cell
+        $(".cell.active").removeClass("error");
+    
+        // Set the "data-value" attribute of the currently selected cell to 0
+        $(".cell.active").attr("data-value", 0);
+    
+        // Call the "checkErrors" function
+        checkErrors();
+    }
+}
+
+function removeActive(e) 
+{
+    // Select the "board" element
+    var container = $(".board");
+
+    // Select the elements with the "numbers" class
+    var numbers = $(".numbers");
+
+    // Check if the target of the click is not the container or a descendant of the container, and if it is not the numbers grid or a descendant of the numbers grid
+    if (!container.is(e.target) && !numbers.is(e.target) && container.has(e.target).length === 0 && numbers.has(e.target).length === 0) 
+    {
+        // Remove the "active" class from all cells
+        $(".cell").removeClass("active");
+    
+        // Remove the "soft-active" class from all cells
+        $(".cell.soft-active").removeClass("soft-active");
+    }
+}
+
+function checkErrors() 
+{
+    // iterate through all columns
+    for (var i = 1; i <= 9; i++) 
+    {
+
+        var cellsArray = [];
+        var errors = [];
+  
+        // iterate through all cells in the current column
+        for (var j = 1; j <= 9; j++) 
+        {
+            var cellValue = $("[data-row='" + j + "'][data-column='" + i + "']").data("value");
+    
+            if (cellValue != 0) 
+            {
+                if (cellsArray.includes(cellValue)) 
+                {
+                    errors.push(cellValue);
+                }
+
+                cellsArray.push(cellValue);
+            }
+        }
+
+        console.log(errors);
+    }
+}
+  
